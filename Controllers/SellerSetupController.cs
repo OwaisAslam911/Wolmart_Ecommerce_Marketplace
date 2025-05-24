f2d676bf1a400e9e579229b251181d2981bc3df3
OwaisAslam911 <owaisaslams911@gmail.com>
2025-05-24 11:42:47 -0700

    seller setup started

---
+++ Controllers/SellerSetupController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Wolmart_Ecommerce_Marketplace.Controllers
+{
+    public class SellerSetupController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult RegisterAsSeller()
+        {
+            return View();
+        }
+    }
+}
